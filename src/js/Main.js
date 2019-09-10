a5cae3b755cc7152e5f1131d1dfd096fa8d400d1
sebastian velasquez <sebasm4a1@hotmail.com>
2019-09-09 20:52:07 -0500

    alargue el switch

--- src/js/Main.js
+++ src/js/Main.js
@@ -1,9 +1,16 @@
 let pantalla;
-function setup(){
+function setup() {
 
 }
-function draw(){
-    switch(pantalla){
+function draw() {
+    switch (pantalla) {
         case 1:
             break;
-    }
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+
+    }}
